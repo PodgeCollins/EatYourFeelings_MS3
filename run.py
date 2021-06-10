bacff4d4758e8179eff10156066207747ad90d58
PodgeCollins <collinspadraic97@gmail.com>
2021-06-10 04:28:50 +0000

    Create recipes.json, ingredients.html, update run.py and deployment to heroku

--- run.py
+++ run.py
@@ -1,29 +1,52 @@
 import os
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
+    return render_template("index.html", header="Index Page")
 
 
 @app.route("/recipes")
 def recipes():
-    return render_template("recipes.html")
+    data = []
+    with open("data/recipes.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("recipes.html", header="Perfect Recipes",  subheader="for Gluttony & Self Loathing", recipe=data)
 
 
-@app.route("/login")
+@app.route("/recipes/<ingredients_prep>")
+def recipes_recipe(ingredients_prep):
+    recipe = {}
+    with open("data/recipes.json", "r") as json_data:
+        data=json.load(json_data)
+        for obj in data:
+            if obj["url"] == ingredients_prep:
+                recipe = obj
+    return render_template("ingredients.html", ingredients=recipe, header="Lets Get Cooking")
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        flash("Welcome back, {}!".format(
+            request.form.get("name")))
+    return render_template("login.html", header="Log In Below",  subheader="We've Missed You!")
 
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return render_template("signup.html")
+    if request.method == "POST":
+        flash("Welcome to the Family, {}!".format(
+            request.form.get("name")))
+    return render_template("signup.html", header="Create an Account!")
 
 
 if __name__ == "__main__":
